63d466c757eeec667e7b95813a6882026b6f16c5
jixinchan <1209980074@qq.com>
2019-02-26 17:11:39 +0800

    rectangle caculate

---
+++ rectangle.js
@@ -0,0 +1,15 @@
+$(function(){
+  var $width=$('#width'),
+      $length=$('#length'),
+      $btnCal=$('#caculate'),
+      $area=$('#area'),
+      $perimeter=$('#perimeter');
+
+$btnCal.click(function(){
+  var w=Number($width.val()),l=Number($length.val());
+  var p=2*(w+l);
+  var a=w*l;
+  $area.val(a);
+  $perimeter.val(p);
+});
+});
