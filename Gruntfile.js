90bc3443afa3fd16716221ac87d16c234ad8213d
jixinchan <1209980074@qq.com>
2019-03-17 21:26:00 +0800

    矩形计算器v5.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,7 @@ module.exports = function (grunt) {
         run: true,
         reporter: 'Spec'
       }
+        
     },
     htmlmin: {
       options: {
@@ -34,9 +35,10 @@ module.exports = function (grunt) {
         preserveLineBreaks: false
       },
       files: {
-        src: './index.html',
+        src: 'dist/index.html',
         dest: 'dist/index.html'
       }
+        
     },
     cssmin: {
       'dist/rectangle.css': 'rectangle.css'
@@ -44,22 +46,50 @@ module.exports = function (grunt) {
     uglify: {
       release:{
         files: {
-          'dist/rectangle.js': 'rectangle.js',
-          'dist/calc.js': 'calc.js'
+          'dist/bundle.min.js': 'dist/bundle.js',
         }
       }
+    },
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+        dest: 'dist'
+      }
+    },
+    usemin: {
+      html: ['dist/index.html']
+    },
+    concat: {
+      options: {
+        separator: ';'
+      },
+      js: {
+        src: ['rectangle.js', 'calc.js'],
+        dest: 'dist/bundle.js'
+      }
+    },
+    clean: ['dist/bundle.js', '.tmp'],
+    copy: {
+      html: {
+        src: './index.html',
+        dest: './dist/index.html'
+      }
     }
-    
   });
 
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-mocha');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
   grunt.registerTask('unitTest', ['mocha']);
-  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
+  grunt.registerTask('release', ['copy', 'useminPrepare', 'concat', 'uglify', 'usemin', 'cssmin', 'htmlmin', 'clean']);
+
 };
--- test/calc.test.js
+++ test/calc.test.js
@@ -1,11 +1,34 @@
-var expect = chai.expect;
-describe('计算矩形面积测试',function(){
-  it('长是10，宽是2，面积是20',function(){
-    var rect=rectangle();
-    expect(rect.area(10,2)).to.be.equal(20);
+var expect = chai.expect;//eslint-disable-line no-undef    
+describe('计算矩形面积测试',function(){//eslint-disable-line no-undef    
+  it('长是10，宽是2，面积是20',function(){//eslint-disable-line no-undef    
+    var rect=new rectangle(10,2);//eslint-disable-line no-undef    
+    expect(rect.area()).to.be.equal(20);
   });
-  it('长是8，宽是3，周长是22',function(){
-    var rect=rectangle();
-    expect(rect.perimeter(8,3)).to.be.equal(22);
-  })
+  it('长是0.8，宽是0.3，面积是0.24',function(){//eslint-disable-line no-undef    
+    var rect=new rectangle(0.8,0.3);//eslint-disable-line no-undef    
+    expect(rect.area()).to.be.equal(0.24);
+  });
+  it('宽度和高度是整数字符串', function() {//eslint-disable-line no-undef
+    var r = new rectangle('13', '2');//eslint-disable-line no-undef
+    expect(r.area()).to.be.equal(26);            
+  });
+});
+
+describe('矩形计算器周长计算功能测试套件', function(){//eslint-disable-line no-undef
+  it('宽度和高度是整数', function(){//eslint-disable-line no-undef
+    var r = new rectangle(4, 5);//eslint-disable-line no-undef
+    expect(r.perimeter()).to.be.equal(18);             
+  });
+
+  it('宽度和高度是小数', function() {//eslint-disable-line no-undef
+    var r = new rectangle(0.3, 0.8);//eslint-disable-line no-undef
+    expect(r.perimeter()).to.be.equal(2.2000000000000001);              
+  });
+
+  it('宽度和高度是整数字符串', function() {//eslint-disable-line no-undef
+    var r = new rectangle('4', '5');//eslint-disable-line no-undef
+    expect(r.perimeter()).to.be.equal(18);
+              
+  });
+
 });
