e43297946c27beda1e0a554d1086d28067a45e1f
jixinchan <1209980074@qq.com>
2019-03-27 16:21:29 +0800

    rectangle v6.0 校验

--- calc.js
+++ calc.js
@@ -2,15 +2,32 @@
 function rectangle(width,height) {//eslint-disable-line no-unused-vars
   var w = Number(width),
       h = Number(height);
-
   this.area = function() {
     return w * h;
-            
   };
-
   this.perimeter = function() {
-    return 2 * (w + h);
-            
+    return 2 * (w + h);          
+  };
+}
+
+function valid(data){
+  var result = {
+    isOK:false,
+    reason:''
   };
 
+  if(data==''){
+    result.reason='不能为空！';
+    return result;                
+  }else if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-])?(\d+)?$/.test(data)){
+    result.reason='不是数字！';
+    return result;                
+  }else if(Number(data)<0){
+    result.reason='不能为负！';
+    return result;                 
+  }else{
+    result.reason='';
+    result.isOK=true;
+    return result;
+  }
 }
--- rectangle.js
+++ rectangle.js
@@ -3,11 +3,41 @@ $(function(){
       $length=$('#length'),
       $btnCal=$('#caculate'),
       $area=$('#area'),
-      $perimeter=$('#perimeter');
+      $perimeter=$('#perimeter'),
+      $widthValidation=$('#width-validation'),
+      $lengthValidation=$('#length-validation');
 
   $btnCal.click(function(){
+    var w=$width.val(),l=$length.val();
+    if(l===''){
+      $lengthValidation.html('高度不能为空！');
+    }
+    if(w===''){
+      $widthValidation.html('宽度不能为空！');
+      return;
+    }
     var rect = new rectangle($width.val(),$length.val());//eslint-disable-line no-undef
     $area.val(rect.area());
     $perimeter.val(rect.perimeter());
   });
+  $length.focusout(function(){
+    var l = $length.val();
+    var result = valid(l);
+    if(!result.isOK){
+      $lengthValidation.html('长度'+result.reason);
+    }else{
+      $lengthValidation.html('');
+    }
+     
+  });
+  $width.focusout(function(){
+    var w = $width.val();
+    var result = valid(w);
+    if(!result.isOK){
+      $widthValidation.html('宽度'+result.resaon);
+    }else{
+      $widthValidation.html('');
+     }
+  });
+
 });
